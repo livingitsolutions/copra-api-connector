4aa77b98b1df6c7819d8934090db6659e58e443f
jmaasin-cmd <jmaasin@southernleytestateu.edu.ph>
2026-04-10 15:04:42 +0800

    Update sensor data keys to uppercase

--- main.py
+++ main.py
@@ -211,9 +211,9 @@ async def run_test_loop():
                 payload = {
                     "moisture": predictions_in["Moisture"],
                     "temperature": predictions_in["Temperature"],
-                    "r": sensor_data["r"],
-                    "g": sensor_data["g"],
-                    "b": sensor_data["b"],
+                    "r": sensor_data["R"],
+                    "g": sensor_data["G"],
+                    "b": sensor_data["B"],
                     "svm_grade": predictions_out.get("SVM"),
                     "rf_grade": predictions_out.get("Random Forest"),
                     "knn_grade": predictions_out.get("KNN"),
